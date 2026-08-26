db370827294256750c2403add0d87d81117086a5
Teja <176801608+Sriteja1204@users.noreply.github.com>
2026-08-26 14:49:31 +0530

    another commnit

--- 90-components/bootstrap.sh
+++ 90-components/bootstrap.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-component=$1 #catalogue
+component=$1 #mongodb
 environment=$2 #dev
-app_version=$3
 dnf install ansible -y
 mkdir -p /var/log/roboshop/
 chown -R ec2-user:ec2-user /var/log/roboshop
@@ -10,7 +9,7 @@ chmod -R 755 /var/log/roboshop
 touch /var/log/roboshop/ansible.log
 
 cd /home/ec2-user
-git clone https://github.com/daws-90s/roboshop-ansible-v3.git
-cd roboshop-ansible-v3
+git clone https://github.com/Sriteja1204/roboshop-ansible-v1.git
+cd roboshop-ansible-v1
 git pull
-ansible-playbook -e component=$component -e env=$environment -e app_version=$app_version roboshop.yaml
+ansible-playbook -e component=$component -e env=$environment roboshop.yaml
